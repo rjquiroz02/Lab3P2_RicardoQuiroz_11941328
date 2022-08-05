e52b9ca57eeddddbaacc67e961a8bb6a86733403
rjquiroz02 <rjquiroz02@unitec.edu>
2022-08-05 13:40:03 -0600

    Mensaje#1

---
+++ src/lab3p2_ricardoquiroz_11941328/CombSolido.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3p2_ricardoquiroz_11941328;
+
+/**
+ *
+ * @author rjqer
+ */
+public class CombSolido {
+    
+}
