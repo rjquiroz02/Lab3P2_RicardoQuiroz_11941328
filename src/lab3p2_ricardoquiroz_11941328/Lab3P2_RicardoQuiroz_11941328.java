fbd2d4f10a2bc2f19f05ee1de86d40eace3bc509
rjquiroz02 <rjquiroz02@unitec.edu>
2022-08-05 13:58:38 -0600

    Mensaje#2

--- src/lab3p2_ricardoquiroz_11941328/Lab3P2_RicardoQuiroz_11941328.java
+++ src/lab3p2_ricardoquiroz_11941328/Lab3P2_RicardoQuiroz_11941328.java
@@ -5,17 +5,70 @@
  */
 package lab3p2_ricardoquiroz_11941328;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rjqer
  */
 public class Lab3P2_RicardoQuiroz_11941328 {
-
+static Scanner lea = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        boolean cent;
+        int menu=0;
+        while (menu != 0) {            
+            cent=false;
+            System.out.println("0. Salir");
+            System.out.println("1. Crear cohete");
+            System.out.println("2. Crear planeta");
+            System.out.println("3. Editar cohete");
+            System.out.println("4. Editar planeta");
+            System.out.println("5. Listar cohetes");
+            System.out.println("6. Listar planetas");
+            System.out.println("7. Prueba");
+            System.out.print("Ingrese la opcion que desea: ");
+            menu = lea.nextInt();
+            System.out.println();
+            if (menu==1) {
+                
+                
+                cent=true;
+            }
+            if (menu==2) {
+                
+                
+                cent=true;
+            }
+            if (menu==3) {
+                
+                
+                cent=true;
+            }
+            if (menu==4) {
+                
+                
+                cent=true;
+            }
+            if (menu==5) {
+                
+                
+                cent=true;
+            }
+            if (menu==6) {
+                
+                
+                cent=true;
+            }
+            if (menu==7) {
+                
+                
+                cent=true;
+            }
+            
+        }//while
+    }//void main
     
-}
+}//public class
