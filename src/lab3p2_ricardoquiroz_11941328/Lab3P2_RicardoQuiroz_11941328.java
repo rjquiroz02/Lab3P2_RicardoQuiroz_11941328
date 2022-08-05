2fb2646292ac64e409b74daf82a9a46bc06dfd7a
rjquiroz02 <rjquiroz02@unitec.edu>
2022-08-05 13:19:42 -0600

    Comienzo

---
+++ src/lab3p2_ricardoquiroz_11941328/Lab3P2_RicardoQuiroz_11941328.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3p2_ricardoquiroz_11941328;
+
+/**
+ *
+ * @author rjqer
+ */
+public class Lab3P2_RicardoQuiroz_11941328 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
